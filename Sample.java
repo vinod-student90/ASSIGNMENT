6929c67c85baffe5c9e6c39273382cbfe150e2c0
vinod-student90 <vinodmca851@gmail.com>
2024-09-19 00:04:58 +0530

    Add files via upload

---
+++ Sample.java
@@ -0,0 +1,21 @@
+package muthootworld;
+
+public class Sample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		float version= 91.0F;
+		String browser = "chrome";
+	    int year =2008;
+		char logo ='c';
+		System.out.println("version of chrome:" +version);
+		System.out.println("BROWSER:" +browser);
+		System.out.println("BROWSER RELEASE:" +year);
+		System.out.println("BROWSER logo:" +logo);
+		
+		
+
+	}
+
+}
